05fb92b8429a329fd2d92a4226f6ad71ecad99d0
PieroMarraffa <pieromarraffa.business@gmail.com>
2022-03-14 12:36:22 +0100

    Primo Progetto creato in VS

---
+++ PrimoProgetto/PrimoProgetto/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PrimoProgetto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int x = 1;
+            Console.WriteLine(x);
+        }
+    }
+}
