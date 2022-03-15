7824e88bea0487c4f564f2156c9328919d096cf8
PieroMarraffa <pieromarraffa.business@gmail.com>
2022-03-15 16:45:58 +0100

    nuova classe

---
+++ Classe2/EmptyCSharpFile.cs
@@ -0,0 +1,10 @@
+﻿/*AUTORE: PIERO MARRAFFA
+ DATA: 15-3-2022*/
+using System;
+
+public class Prova{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World");
+    }
+}
