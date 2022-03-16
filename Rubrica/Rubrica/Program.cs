6559961f9804b4271fbfe92d6ab390a66604e17a
PieroMarraffa <pieromarraffa.business@gmail.com>
2022-03-16 14:40:06 +0100

    classe rubrica

---
+++ Rubrica/Rubrica/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Rubrica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inserisci il tuo Nome: ");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Inserisci il tuo Cognome: ");
+            string cognome = Console.ReadLine();
+            Console.WriteLine("Inserisci la tua Email: ");
+            string email = Console.ReadLine();
+            Console.WriteLine("Inserisci il tuo Cellulare: ");
+            long cellulare = long.Parse(Console.ReadLine());
+
+            gestioneRubrica rubrica = new gestioneRubrica(nome, cognome, email, cellulare);
+            Console.WriteLine(rubrica.toString());
+
+        }
+    }
+}
---
+++ Rubrica/Rubrica/gestioneRubrica.cs
@@ -0,0 +1,79 @@
+﻿using System;
+namespace Rubrica
+{
+    public class gestioneRubrica
+    {
+
+        //ATTRIBUTI
+        private string nome, cognome, email;
+        private long cellulare;
+
+        //COSTRUTTORE, GETTER AND SETTER
+
+        public gestioneRubrica()
+        {
+
+        }
+
+        public gestioneRubrica(string nome, string cognome)
+        {
+            this.nome = nome;
+            this.cognome = cognome;
+        }
+
+        public gestioneRubrica(string nome, string cognome, string email, long cellulare)
+        {
+            this.nome = nome;
+            this.cognome = cognome;
+            this.email = email;
+            this.cellulare = cellulare;
+        }
+
+        public void setNome(string dato)
+        {
+            this.nome = dato;
+        }
+
+        public void setCognome(string dato)
+        {
+            this.cognome = dato;
+        }
+
+        public void setEmail(string dato)
+        {
+            this.email = dato;
+        }
+
+        public void setCellulare(long dato)
+        {
+            this.cellulare = dato;
+        }
+
+        public string getNome()
+        {
+            return nome;
+        }
+
+        public string getCognome()
+        {
+            return cognome;
+        }
+
+        public string getEmail()
+        {
+            return email;
+        }
+
+        public long getCellulare()
+        {
+            return cellulare;
+        }
+
+        //METODI UTILI
+
+        public string toString()
+        {
+            return "I tuoi dati sono:   Nome: " + this.nome + "    Cognome: " + this.cognome + "    Email: " + this.email + "    Cellulare: " + this.cellulare;
+        }
+    }
+}
