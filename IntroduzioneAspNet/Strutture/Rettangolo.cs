25970ea1a3c8b756cb11024d77656a7041048f46
PieroMarraffa <pieromarraffa.business@gmail.com>
2022-03-31 17:33:18 +0200

    boh pomeriggio cazzeggio

---
+++ IntroduzioneAspNet/Strutture/Rettangolo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Strutture
+{
+    public struct Rettangolo
+    {
+        public int Base;
+        public int Altezza;
+
+        public Rettangolo(int a, int b)
+        {
+            this.Altezza = a;
+            this.Base = b;
+        }
+
+        public int Area()
+        {
+            return (Base * Altezza);
+        }
+    }
+}
